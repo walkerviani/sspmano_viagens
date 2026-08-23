6dc5dfdb39246ca8e9e8bd66cf22d3dd87dbaed6
walkerviani <walkeryslan@gmail.com>
2026-08-23 19:48:43 -0300

    adicionado vermelhoClaro

--- lib/utils/cores_app.dart
+++ lib/utils/cores_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class CoresApp {
   static const vermelho = Color(0xFFba2c2e);
+  static const vermelhoClaro = Color(0xFFbd0808);
   static const branco = Colors.white;
   static const vinho = Color(0xFFa22628);
   static const verdeClaro = Color(0xFF3a8a14);
