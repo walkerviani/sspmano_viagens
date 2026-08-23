3709ab1d6d4e50df036a9f6ddcb7784e2a9e6eab
walkerviani <walkeryslan@gmail.com>
2026-08-23 14:56:36 -0300

    adicionado classe de cores

---
+++ lib/utils/cores_app.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CoresApp {
+  static const vermelho = Color(0xFFba2c2e);
+  static const branco = Colors.white;
+  static const vinho = Color(0xFFa22628);
+}
