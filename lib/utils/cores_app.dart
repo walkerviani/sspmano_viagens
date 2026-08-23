86cb640044d31c04a0febde1313315c31970d78b
walkerviani <walkeryslan@gmail.com>
2026-08-23 20:20:27 -0300

    adicionado cor laranja

--- lib/utils/cores_app.dart
+++ lib/utils/cores_app.dart
@@ -8,4 +8,5 @@ class CoresApp {
   static const verdeClaro = Color(0xFF3a8a14);
   static const cinzaClaro = Color(0xFFd1d1d1);
   static const grafite = Color(0xFF41424C);
+  static const laranja = Color(0xFFd66104);
 }
