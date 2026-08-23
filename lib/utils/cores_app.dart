2d285fd76cdf76f4105d70a9b88f5be39fce8443
walkerviani <walkeryslan@gmail.com>
2026-08-23 15:13:00 -0300

    adicionado cor verdeClaro

--- lib/utils/cores_app.dart
+++ lib/utils/cores_app.dart
@@ -4,4 +4,5 @@ class CoresApp {
   static const vermelho = Color(0xFFba2c2e);
   static const branco = Colors.white;
   static const vinho = Color(0xFFa22628);
+  static const verdeClaro = Color(0xFF3a8a14);
 }
