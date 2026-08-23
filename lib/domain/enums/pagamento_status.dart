87769fd5f93fd432e605888c041520ee616604a1
walkerviani <walkeryslan@gmail.com>
2026-08-23 20:19:56 -0300

    removido enum de pagamento

--- lib/domain/enums/pagamento_status.dart
+++
@@ -1,9 +0,0 @@
-enum PagamentoStatus {
-  pendente(1, 'PENDENTE'),
-  pago(2, 'PAGO');
-
-  final int id;
-  final String pagamentoStatus;
-
-  const PagamentoStatus(this.id, this.pagamentoStatus);
-}
