d3e29e0bdb44ecbc849f5f5c4759514fdadbcc07
walkerviani <walkeryslan@gmail.com>
2026-08-23 15:13:13 -0300

    adicionado página de pessoas

---
+++ lib/presentation/pessoas_list_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sspmano_viagens/utils/cores_app.dart';
+
+class PessoasListScreen extends StatefulWidget {
+  const PessoasListScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _PessoasListScreenState();
+}
+
+class _PessoasListScreenState extends State<PessoasListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pessoas', style: GoogleFonts.poppins(fontSize: 28)),
+        backgroundColor: CoresApp.vermelho,
+        foregroundColor: CoresApp.branco,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: CoresApp.verdeClaro,
+                foregroundColor: CoresApp.branco,
+                minimumSize: Size(double.infinity, 70),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.person_add, size: 40),
+                  const SizedBox(width: 10),
+                  Text(
+                    'Adicionar pessoa',
+                    style: GoogleFonts.poppins(
+                      color: CoresApp.branco,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
