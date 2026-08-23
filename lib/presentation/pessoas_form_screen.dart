6c7c3db90a647d4319695c709dad04f955bf99ca
walkerviani <walkeryslan@gmail.com>
2026-08-23 15:42:49 -0300

    adicionado tela de criação/edição de pessoas

---
+++ lib/presentation/pessoas_form_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sspmano_viagens/utils/cores_app.dart';
+
+class PessoasFormScreen extends StatefulWidget {
+  final bool modoEdicao;
+  const PessoasFormScreen({super.key, required this.modoEdicao});
+
+  @override
+  State<StatefulWidget> createState() => _PessoasFormScreenState();
+}
+
+class _PessoasFormScreenState extends State<PessoasFormScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.modoEdicao ? 'Editar Pessoa' : 'Adicionar pessoa',
+          style: GoogleFonts.poppins(fontSize: 28),
+        ),
+        backgroundColor: CoresApp.vermelho,
+        foregroundColor: CoresApp.branco,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(12),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'O nome não pode estar vazio';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  alignLabelWithHint: true,
+                  hintText: 'Nome',
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'O CPF não pode estar vazio';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  alignLabelWithHint: true,
+                  hintText: 'CPF',
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'O Telefone não pode estar vazio';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  alignLabelWithHint: true,
+                  hintText: 'Telefone',
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    return;
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: CoresApp.verdeClaro,
+                  foregroundColor: CoresApp.branco,
+                  minimumSize: Size(double.infinity, 70),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                child: Text(
+                  widget.modoEdicao ? 'Editar' : 'Adicionar',
+                  style: GoogleFonts.poppins(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
