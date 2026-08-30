67411964d5c51252243ba37b6db9b8f3fdcc5b2f
walkerviani <walkeryslan@gmail.com>
2026-08-30 13:49:22 -0300

    adicionado viewmodel do form de excursões

---
+++ lib/presentation/viewmodels/excursoes_form_viewmodel.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:sspmano_viagens/domain/entities/excursao.dart';
+import 'package:sspmano_viagens/domain/repositories/excursao_repository.dart';
+
+class ExcursoesFormViewmodel extends ChangeNotifier {
+  final ExcursaoRepository _repository;
+
+  ExcursoesFormViewmodel(this._repository);
+  bool estaCarregando = false;
+  String? mensagemErro;
+  Excursao? excursao;
+
+  Future<void> carregarExcursao(int id) async {
+    mensagemErro = null;
+    estaCarregando = true;
+    notifyListeners();
+
+    try {
+      excursao = await _repository.listarPorId(id);
+    } catch (e) {
+      mensagemErro = 'Erro ao carregar a excursão';
+    } finally {
+      estaCarregando = false;
+      notifyListeners();
+    }
+  }
+
+  Future<bool> salvarExcursao({
+    int? id,
+    required String nome,
+    required DateTime data,
+    required TimeOfDay hora,
+    required int qntAssentos,
+    required int idStatus,
+  }) async {
+    mensagemErro = null;
+
+    if (nome.trim().isEmpty) {
+      mensagemErro = 'O nome não pode estar vazio';
+      return false;
+    }
+    if (nome.length > 100) {
+      mensagemErro = 'O Nome precisa ser menor que 100 caracteres';
+      return false;
+    }
+
+    DateTime dataHora = DateTime(
+      data.day,
+      data.month,
+      data.year,
+      hora.minute,
+      hora.hour,
+    );
+
+    estaCarregando = true;
+    notifyListeners();
+
+    try {
+      Excursao excursao = Excursao(id, nome, dataHora, qntAssentos);
+      if (id != null) {
+        await _repository.atualizar(excursao);
+      } else {
+        await _repository.criar(excursao);
+      }
+      return true;
+    } catch (e) {
+      mensagemErro = 'Houve um erro ao salvar a excursão';
+      return false;
+    } finally {
+      estaCarregando = false;
+      notifyListeners();
+    }
+  }
+}
