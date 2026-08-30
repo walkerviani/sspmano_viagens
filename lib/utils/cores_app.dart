1a9846483d852a6afba1ccdf6cc0c16746ed67a4
walkerviani <walkeryslan@gmail.com>
2026-08-30 15:41:51 -0300

    adicionado cores cinzaGrafite e azulPetroleo

--- lib/utils/cores_app.dart
+++ lib/utils/cores_app.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class CoresApp {
   static const vermelho = Color(0xFFba2c2e);
   static const vermelhoClaro = Color(0xFFbd0808);
-  static const branco = Colors.white;
   static const vinho = Color(0xFFa22628);
+  static const cinzaGrafite = Color(0xFF383e41);
+  static const azulPetroleo = Color(0xFF173649);
   static const verdeClaro = Color(0xFF3a8a14);
   static const cinzaClaro = Color(0xFFd1d1d1);
   static const grafite = Color(0xFF41424C);
   static const laranja = Color(0xFFd66104);
   static const azulClaro = Color(0xFF2196F3);
+  static const branco = Colors.white;
 }
