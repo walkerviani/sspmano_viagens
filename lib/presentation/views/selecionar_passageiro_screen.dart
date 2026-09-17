c4ef26a28ffcde55afa32be14c2d71f1af3c418e
walkerviani <walkeryslan@gmail.com>
2026-09-17 19:26:57 -0300

    atualiado tela de selecionar assentos para utilizar o viewmodel

--- lib/presentation/views/selecionar_passageiro_screen.dart
+++ lib/presentation/views/selecionar_passageiro_screen.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:sspmano_viagens/domain/entities/pessoa.dart';
+import 'package:sspmano_viagens/presentation/viewmodels/selecionar_passageiro_viewmodel.dart';
 import 'package:sspmano_viagens/utils/cores_app.dart';
 
 class SelecionarPassageiroScreen extends StatefulWidget {
-  const SelecionarPassageiroScreen({super.key});
+  final int numAssento;
+  final int idVeiculo;
+  const SelecionarPassageiroScreen({
+    super.key,
+    required this.numAssento,
+    required this.idVeiculo,
+  });
 
   @override
   State<StatefulWidget> createState() => _SelecionarPassageiroScreenState();
@@ -12,6 +21,37 @@ class SelecionarPassageiroScreen extends StatefulWidget {
 class _SelecionarPassageiroScreenState
     extends State<SelecionarPassageiroScreen> {
   @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<SelecionarPassageiroViewmodel>().carregarPessoas();
+    });
+  }
+
+  void _vincularPassageiro(Pessoa pessoa) async {
+    final viewmodel = context.read<SelecionarPassageiroViewmodel>();
+    bool sucesso = await viewmodel.vincularPassageiro(
+      pessoa,
+      widget.idVeiculo,
+      widget.numAssento,
+    );
+    if (!mounted) return;
+    if (sucesso) {
+      Navigator.pop(context);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            viewmodel.mensagemErro ?? 'Houve algum erro desconhecido',
+            style: GoogleFonts.poppins(color: CoresApp.branco),
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -19,28 +59,59 @@ class _SelecionarPassageiroScreenState
         backgroundColor: CoresApp.vermelho,
         foregroundColor: CoresApp.branco,
       ),
-      body: Container(padding: EdgeInsets.all(12), child: _cardPassageiro()),
+      body: Container(
+        padding: EdgeInsets.all(12),
+        child: Consumer<SelecionarPassageiroViewmodel>(
+          builder: (context, viewmodel, child) {
+            if (viewmodel.estaCarregando) {
+              return const Center(
+                child: CircularProgressIndicator(color: CoresApp.vermelho),
+              );
+            }
+            if (viewmodel.pessoas.isEmpty) {
+              return Center(
+                child: Text(
+                  'Nenhum usuário encontrado',
+                  style: GoogleFonts.poppins(
+                    color: CoresApp.grafite,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            }
+            return ListView.builder(
+              itemCount: viewmodel.pessoas.length,
+              itemBuilder: ((context, index) =>
+                  _cardPassageiro(viewmodel.pessoas[index])),
+            );
+          },
+        ),
+      ),
     );
   }
 
-  Widget _cardPassageiro() {
+  Widget _cardPassageiro(Pessoa pessoa) {
     return Card(
       child: Padding(
         padding: EdgeInsets.all(5),
         child: ListTile(
           title: Text(
-            'Nome do passageiro',
+            pessoa.nome,
             style: GoogleFonts.poppins(
               fontSize: 15,
               fontWeight: FontWeight.bold,
             ),
           ),
           subtitle: Text(
-            '999.999.999-99 | 19971343538',
+            '${pessoa.cpf} | ${pessoa.telefone}',
             style: GoogleFonts.poppins(fontSize: 15),
           ),
           trailing: IconButton(
-            onPressed: () {},
+            onPressed: () {
+              _vincularPassageiro(pessoa);
+            },
             style: IconButton.styleFrom(
               backgroundColor: CoresApp.verdeClaro,
               shape: RoundedRectangleBorder(
